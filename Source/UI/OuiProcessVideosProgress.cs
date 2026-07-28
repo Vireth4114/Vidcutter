23a2d45954d8d41467e73443bbb998cb11ae919d
Vireth <vireth4114@gmail.com>
2026-07-28 21:08:29 +0200

    feat(video-process): always clean the videos

--- Source/UI/OuiProcessVideosProgress.cs
+++ Source/UI/OuiProcessVideosProgress.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Celeste.Mod.UI;
 using Celeste.Mod.Vidcutter.Utils;
@@ -18,20 +19,24 @@ public class OuiProcessVideosProgress : OuiLoggedProgress {
     
     public override IEnumerator Enter(Oui from) {
         Init<OuiModOptions>(Dialog.Clean("VIDCUTTER_PROCESS_TITLE"), new Task(() => {
-            int clipIdx = 1;
-            int rowIdx = 1;
-        
-            using (StreamWriter clipsIndexWriter = new StreamWriter(FileUtils.ClipsIndexFile)) {
-                foreach (LevelInAVideo levelInAVideo in _rowsToProcess) {
-                    LogLine(Dialog.Clean("VIDCUTTER_PROCESSINGVIDEO") + $" {levelInAVideo.VideoName} ({levelInAVideo.Level}) ({rowIdx++}/{_rowsToProcess.Count})");
-                    clipIdx = ProcessRow(levelInAVideo, clipsIndexWriter, clipIdx);
-                }
-            }
+            try {
+                int clipIdx = 1;
+                int rowIdx = 1;
             
-            ConcatAndClean(clipIdx);
+                using (StreamWriter clipsIndexWriter = new StreamWriter(FileUtils.ClipsIndexFile)) {
+                    foreach (LevelInAVideo levelInAVideo in _rowsToProcess) {
+                        LogLine(Dialog.Clean("VIDCUTTER_PROCESSINGVIDEO") + $" {levelInAVideo.VideoName} ({levelInAVideo.Level}) ({rowIdx++}/{_rowsToProcess.Count})");
+                        clipIdx = ProcessRow(levelInAVideo, clipsIndexWriter, clipIdx);
+                    }
+                }
+                
+                Concatenate();
 
-            if (_deleteAfterProcess) {
-                LogManager.DeleteLogs(_rowsToProcess);
+                if (_deleteAfterProcess) {
+                    LogManager.DeleteLogs(_rowsToProcess);
+                }
+            } finally {
+                Clean();
             }
         }), 100);
 
@@ -64,14 +69,14 @@ public class OuiProcessVideosProgress : OuiLoggedProgress {
         }
         return clipIdx;
     }
-    
-    private void ConcatAndClean(int videoCount) {
+
+    private void Concatenate() {
         string output = VideoCreation.GetOutputVideoName(_rowsToProcess[0].Level);
         FFmpegUtils.ConcatenateClipsFromIndexFilePath(FileUtils.ClipsIndexFile, output);
+    }
 
+    private void Clean() {
         File.Delete(Path.Combine(FileUtils.ClipsIndexFile));
-        for (int i = 1; i < videoCount; i++) {
-            File.Delete(Path.Combine(FileUtils.VidcutterWorkingDirectory, $"{i}.mp4"));
-        }
+        Directory.GetFiles(FileUtils.VidcutterWorkingDirectory, "*.mp4").ToList().ForEach(File.Delete);
     }
 }
